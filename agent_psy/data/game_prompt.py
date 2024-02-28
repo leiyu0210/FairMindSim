f7fefd02e78b15ccec529804ec7b4c613c6d8703
leiyu0210 <108879926+leiyu0210@users.noreply.github.com>
2024-02-28 21:00:46 +0800

    Update game_prompt.py

--- agent_psy/data/game_prompt.py
+++ agent_psy/data/game_prompt.py
@@ -2,5 +2,4 @@ GAME_PROMPT = "In this experimental game, there are three players: player1, play
 
 
 PROCESS_PROMPT = """After the allocation is complete, please evaluate your emotional state based on the 20 emotional dimensions of the PANAS scale. Then, you will make a judgment: if you accept the allocation, you will receive a reward of 1 RMB; if you reject the allocation, you will receive nothing and player1's income will be reduced to zero, while player2's income remains unchanged. Regardless of your decision, please output your anticipated emotional state after making your judgment. After rendering your judgment, please provide your decision and the actual scores for your emotional state on the PANAS scale. The game is now starting, please get ready. This is the {x} trial, player1 receives 3 RMB, and then leaves itself {y} RMB, which is allocated to player2 {z} RMB. You must response in such format:
-Observed allocation scheme: [describe scheme].
 """
